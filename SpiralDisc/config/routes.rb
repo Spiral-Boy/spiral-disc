090b36c0be75c6018c084cb64a79bc66f49d6589
shun <1018zaqzaq1224@gmail.com>
2017-12-21 21:42:27 +0900

    Top手直し

--- SpiralDisc/config/routes.rb
+++ SpiralDisc/config/routes.rb
@@ -29,5 +29,7 @@ Rails.application.routes.draw do
 
   resources :order, :only => [:create]
 
+  root 'products#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
