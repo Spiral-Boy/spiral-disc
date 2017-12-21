da4d5e8a160ff0ab48d15c5c3d76fa941609af1e
shun <styk.hd.2738@gmail.com>
2017-12-21 12:40:42 +0000

    合体用

--- SpiralDisc/app/controllers/carts_controller.rb
+++ SpiralDisc/app/controllers/carts_controller.rb
@@ -1,7 +1,6 @@
 class CartsController < ApplicationController
 
 	def index
-		@carts = Cart.all
 	end
 
 	def create
--- SpiralDisc/app/controllers/users_controller.rb
+++ SpiralDisc/app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 	end
 
 	private
-		def params
+		def user_params
 			params.require(:user).permit(:name, :name_kana, :postal_code, :street_address, :phone_number, :email, :password)
 		end
 end
